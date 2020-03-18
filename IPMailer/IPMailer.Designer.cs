c7c56806f2646efb2ea9a7a64f5c009034f2a560
PeterFromSweden <peterfromswe884@gmail.com>
2020-03-18 09:56:23 +0100

    Added a C# project that uses outlook for sending e-mails without credentials.

---
+++ IPMailer/IPMailer.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace IPMailer
+{
+    partial class IPMailer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(IPMailer));
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.lblIP = new System.Windows.Forms.Label();
+            this.tmrAuto = new System.Windows.Forms.Timer(this.components);
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.chkAuto = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(16, 29);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(529, 20);
+            this.txtEmail.TabIndex = 3;
+            this.txtEmail.Text = "example@securemeters.com";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "e-mail address recipient";
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(16, 146);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 5;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // lblIP
+            // 
+            this.lblIP.AutoSize = true;
+            this.lblIP.Location = new System.Drawing.Point(455, 13);
+            this.lblIP.Name = "lblIP";
+            this.lblIP.Size = new System.Drawing.Size(27, 13);
+            this.lblIP.TabIndex = 6;
+            this.lblIP.Text = "lblIP";
+            // 
+            // tmrAuto
+            // 
+            this.tmrAuto.Enabled = true;
+            this.tmrAuto.Interval = 60000;
+            this.tmrAuto.Tick += new System.EventHandler(this.tmrAuto_Tick);
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(16, 64);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ReadOnly = true;
+            this.txtLog.Size = new System.Drawing.Size(529, 76);
+            this.txtLog.TabIndex = 7;
+            this.txtLog.Text = "txtLog";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "IPMailer";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            // 
+            // chkAuto
+            // 
+            this.chkAuto.AutoSize = true;
+            this.chkAuto.Location = new System.Drawing.Point(109, 150);
+            this.chkAuto.Name = "chkAuto";
+            this.chkAuto.Size = new System.Drawing.Size(144, 17);
+            this.chkAuto.TabIndex = 8;
+            this.chkAuto.Text = "Autoupdate every minute";
+            this.chkAuto.UseVisualStyleBackColor = true;
+            // 
+            // IPMailer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(563, 182);
+            this.Controls.Add(this.chkAuto);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.lblIP);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtEmail);
+            this.Name = "IPMailer";
+            this.Text = "IPMailer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.IPMailer_Shown);
+            this.Resize += new System.EventHandler(this.IPMailer_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Label lblIP;
+        private System.Windows.Forms.Timer tmrAuto;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.CheckBox chkAuto;
+    }
+}
+
---
+++ IPMailer/IPMailer.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IPMailer
+{
+    public partial class IPMailer : Form
+    {
+        public IPMailer()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtEmail.Text = Properties.Settings.Default.email;
+            lblIP.Text = Properties.Settings.Default.ip;
+            chkAuto.Checked = Properties.Settings.Default.auto;
+            txtLog.Text = "";
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            btnOK.Enabled = false;
+            btnOK.Text = "Running";
+            txtLog.Text = "";
+            Application.DoEvents();
+            
+            // Get IP address
+            try
+            {
+                Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                s.Connect("8.8.8.8", 80);
+                IPAddress ipAddr = ((IPEndPoint)s.LocalEndPoint).Address;
+                lblIP.Text = ipAddr.ToString();
+                txtLog.Text += "Present IP: " + lblIP.Text + Environment.NewLine;
+            }
+            catch (Exception ex)
+            {
+                txtLog.Text += ex.ToString() + Environment.NewLine;
+                btnOK.Enabled = true;
+                btnOK.Text = "OK";
+                return;
+            }
+
+
+            if (Properties.Settings.Default.ip != lblIP.Text)
+            {
+                // Send mail
+                try
+                {
+                    Microsoft.Office.Interop.Outlook.Application oApp = new Microsoft.Office.Interop.Outlook.Application();
+                    Microsoft.Office.Interop.Outlook.MailItem email = (Microsoft.Office.Interop.Outlook.MailItem)(oApp.CreateItem(Microsoft.Office.Interop.Outlook.OlItemType.olMailItem));
+                    email.Recipients.Add(txtEmail.Text);
+                    email.Subject = "Updated IP" + lblIP.Text;
+                    email.Body = "See subject...";
+                    ((Microsoft.Office.Interop.Outlook.MailItem)email).Send();
+                    txtLog.Text += "Mail sent." + Environment.NewLine;
+                }
+                catch (Exception ex)
+                {
+                    txtLog.Text += ex.ToString() + Environment.NewLine;
+                    btnOK.Enabled = true;
+                    btnOK.Text = "OK";
+                    return;
+                }
+                chkAuto.Checked = true;
+                tmrAuto.Start();
+
+                Properties.Settings.Default.ip = lblIP.Text;
+                Properties.Settings.Default.email = txtEmail.Text;
+                Properties.Settings.Default.Save();
+            }
+            else
+            {
+                txtLog.Text += "IP unchanged." + Environment.NewLine;
+            }
+            
+            if (Properties.Settings.Default.auto != chkAuto.Checked)
+            {
+                if (chkAuto.Checked)
+                    StartupManager.AddApplicationToCurrentUserStartup();
+                else
+                    StartupManager.RemoveApplicationFromCurrentUserStartup();
+                Properties.Settings.Default.auto = chkAuto.Checked;
+                Properties.Settings.Default.Save();
+            }
+
+            btnOK.Enabled = true;
+            btnOK.Text = "OK";
+        }
+
+        private void tmrAuto_Tick(object sender, EventArgs e)
+        {
+            if(chkAuto.Checked)
+                btnOK_Click(sender, e);
+        }
+
+        private void IPMailer_Resize(object sender, EventArgs e)
+        {
+            if (FormWindowState.Minimized == WindowState)
+                Hide();
+        }
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            Show();
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void IPMailer_Shown(object sender, EventArgs e)
+        {
+            if (chkAuto.Checked)
+            {
+                WindowState = FormWindowState.Minimized;
+                //Hide();
+            }
+        }
+    }
+}
